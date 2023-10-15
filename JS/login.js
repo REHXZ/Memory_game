a972f38e87a28379997e29953dac307deaedc1a1
Rejey <rehxxz@gmail.com>
2023-10-15 16:34:00 +0800

    New bug came up with the login screen so i fixed it

--- JS/login.js
+++ JS/login.js
@@ -1,10 +1,10 @@
 const supabaseclient = supabase.createClient(
-	"httav2://izuagmzuscxemmhlirlk.supabase.co",
+	"https://izuagmzuscxemmhlirlk.supabase.co",
 	"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Iml6dWFnbXp1c2N4ZW1taGxpcmxrIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTY5MDQ2NjcsImV4cCI6MjAxMjQ4MDY2N30.596CiB1jVlRRLQlu0IvViUpoy26Bli_IhW_3lcd2-1U"
 );
-let passfield
-const af = document.getElementById("admin_no");
-const av2_field = document.getElementById("Password-field");
+var id = ''
+const af = document.getElementById("admin_no"); //Admin nummber of user
+const av2_field = document.getElementById("Password-field");// Password entered
 
 const log_in_btn = document.getElementById("log-in-btn");
 const index_sign_up_btn = document.getElementById("sign-up-btn");
@@ -23,6 +23,7 @@ function clear(){
 log_in_btn.addEventListener("click", async function(){
     const av = af.value.trim();
     const av2 = av2_field.value.trim();
+    console.log(av,av2)
     if (av.length == 7 && !isNaN(av)){
         let { data: user, error } = await supabaseclient
         .from('user')
@@ -32,15 +33,19 @@ log_in_btn.addEventListener("click", async function(){
         // Execute the query
         .single(); // Use .single() to retrieve a single record that matches the filter
       // Check for errors
+        console.log(user.password)
       if (av2 !== user.password) {
         var pass = `Incorrect Password or ${av} Admin number`
-        console.log('Error:', error ,`\n${pass}`);
+        console.log('Error:', error ,`\n Or ${pass}`);
         alert.textContent = pass;
       } else {
-        window.location.href = "Main.html";
+        id = av
+        console.log(id)
+        window.location.href = 'Main.html'
     }
   }
   else{
     alert.textContent = "Incorrect Admin number";
+    id = ''
   }
 })
--- JS/test.js
+++ JS/test.js
@@ -1,36 +1,10 @@
-// const supabaseclient = supabase.createClient(
-// 	"https://izuagmzuscxemmhlirlk.supabase.co",
-// 	"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Iml6dWFnbXp1c2N4ZW1taGxpcmxrIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTY5MDQ2NjcsImV4cCI6MjAxMjQ4MDY2N30.596CiB1jVlRRLQlu0IvViUpoy26Bli_IhW_3lcd2-1U"
-// );
+const supabaseclient = supabase.createClient(
+	"https://izuagmzuscxemmhlirlk.supabase.co",
+	"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Iml6dWFnbXp1c2N4ZW1taGxpcmxrIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTY5MDQ2NjcsImV4cCI6MjAxMjQ4MDY2N30.596CiB1jVlRRLQlu0IvViUpoy26Bli_IhW_3lcd2-1U"
+);
 
-// async function getalluser() {
-// 	let { data: user, error } = await supabaseclient
-// 	.from('user')
-// 	.select('admin_no, password') // Select only the admin_no and password columns
-  
-// 	// Filter by admin_no
-// 	.eq('admin_no', '2348935') // Replace 'your_admin_no_value_here' with the admin_no you want to filter
-  
-// 	// Execute the query
-// 	.single(); // Use .single() to retrieve a single record that matches the filter
-  
-//   // Check for errors
-//   if (error) {
-// 	console.error('Error:', error);
-//   } else {
-// 	if (user) {
-// 	  console.log('Password:', user.password);
-// 	} else {
-// 	  console.log('No user found with the specified admin_no.');
-// 	}
-//   }
-// }
-
-// getalluser();
-
-
-log_in_btn.addEventListener("click", async function(){
-    let { data: user, error } = await supabaseclient
+async function getalluser() {
+	let { data: user, error } = await supabaseclient
 	.from('user')
 	.select('admin_no, password') // Select only the admin_no and password columns
   
@@ -45,9 +19,35 @@ log_in_btn.addEventListener("click", async function(){
 	console.error('Error:', error);
   } else {
 	if (user) {
-	  console.log(user.password)
+	  console.log('Password:', user.password);
 	} else {
 	  console.log('No user found with the specified admin_no.');
 	}
   }
-})
+}
+
+getalluser();
+
+
+// log_in_btn.addEventListener("click", async function(){
+//     let { data: user, error } = await supabaseclient
+// 	.from('user')
+// 	.select('admin_no, password') // Select only the admin_no and password columns
+  
+// 	// Filter by admin_no
+// 	.eq('admin_no', '2348935') // Replace 'your_admin_no_value_here' with the admin_no you want to filter
+  
+// 	// Execute the query
+// 	.single(); // Use .single() to retrieve a single record that matches the filter
+  
+//   // Check for errors
+//   if (error) {
+// 	console.error('Error:', error);
+//   } else {
+// 	if (user) {
+// 	  console.log(user.password)
+// 	} else {
+// 	  console.log('No user found with the specified admin_no.');
+// 	}
+//   }
+// })
