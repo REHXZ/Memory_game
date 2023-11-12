513a2f110bc5aecfd4ae1f6688662971fdd123d5
daddy <rehxxz@gmail.com>
2023-11-13 03:01:49 +0800

    added square root side to page

---
+++ JS/libraries.js
@@ -0,0 +1,1 @@
+export const Big = require('big.js');
--- JS/sqr_root.js
+++ JS/sqr_root.js
@@ -1,15 +1,18 @@
-const button = document.getElementById("sqr");
 
-Big.DP = 2000;
+// document.addEventListener("DOMContentLoaded", function() {
+//   const button = document.getElementById("sqr");
+//   const Big = require('../node_modules/big.js');
 
-const sqrt_2 = new Big(17).sqrt().toString();
-console.log(sqrt_2);
+//   button.addEventListener("click", () => {
+//     Big.DP = 2000;
+//     const sqrt_2 = new Big(17).sqrt().toString();
+//     console.log("sqrt_2");
+//     AppendInput(sqrt_2);
+//   });
 
-button.addEventListener("click", () => {
-  AppendInput(sqrt_2);
-});
-
-function AppendInput(inputvalue) {
-  sqr_root.innerHTML = "";
-  sqr_root.innerHTML += `<p>${inputvalue}</p>`;
-}
+//   function AppendInput(inputvalue) {
+//     const number = document.getElementById("number"); // Get the element with id "number"
+//     number.innerHTML = "";
+//     number.innerHTML += `<p>${inputvalue}</p>`;
+//   }
+// });
